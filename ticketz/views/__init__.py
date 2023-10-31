44b818d4058c73970628d26c4a98bbb3678dc5fb
Unknown <none@none>
2023-10-31 07:52:40 -0400

    Added login page

--- ticketz/views/__init__.py
+++ ticketz/views/__init__.py
@@ -1,5 +1,7 @@
 from ticketz.views.ui.index import view_ui_index
+from ticketz.views.ui.login import view_ui_login
 
 def build_views(app):
-    app.register_blueprint(view_ui_index)
+    app.register_blueprint(view_ui_index, url_prefix='/')
+    app.register_blueprint(view_ui_login, url_prefix='/login')
     return app
